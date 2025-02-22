1531cb33cafcab59dcc968e1c411173d81d99479
esteban_terrones <esteban20001118@gmail.com>
2025-02-22 13:55:00 -0500

    avance de endpoint en login

---
+++ src/Controllers/LoginController.ts
@@ -0,0 +1,35 @@
+import express, {Request, Response} from "express"
+const db = require("../DAO/models")
+
+const LoginController = () => {
+    const path: string = "/login";
+
+    const router = express.Router();
+
+    // Endpoint para verificar usuario
+    router.post('/', async (req: Request, res: Response) => {
+        const email = req.body.email
+        const password = req.body.password
+
+        const usuarios = await db.Users.findAll({
+            where: {
+                email: email,
+                password_hash: password
+            }
+        });
+
+        if (usuarios.length > 0) {
+            res.json({
+                msg: ""
+            })
+        } else {
+            res.json({
+                msg: "Error en login"
+            })
+        }
+    });
+
+    return [ path, router ];
+}
+
+export default LoginController;
