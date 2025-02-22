cf243d4573d44855c0a130f598ddef5fba0eb753
gatosimple <20222730@aloe.ulima.edu.pe>
2025-02-22 16:12:24 -0500

    creacion de tablas: usuaios y roles

--- src/Controllers/UsuarioController.ts
+++ src/Controllers/UsuarioController.ts
@@ -2,7 +2,28 @@ import express, {Request, Response} from "express"
 const db = require("../DAO/models")
 
 const UsuarioController = () => {
+    const path: string = "/admin/users";
 
+    const router = express.Router()
+
+    // Endpoint para obtener todos los usuarios
+    router.get('/', async (req: Request, resp: Response) => {
+        const usuarios = await db.User.findAll({
+            include : {
+                model : db.Role,
+                as : "Role",
+                attributes : ["nombre"],
+                required : true
+            }
+        })
+
+        resp.json({
+            msg : "",
+            usuarios : usuarios
+        })
+    })
+
+    return [ path, router ];
 }
 
-export default UsuarioController
+export default UsuarioController;
