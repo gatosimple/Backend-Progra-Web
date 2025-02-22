d22acda096a76cc22fc69aa371d8fa06ddd8586b
esteban_terrones <esteban20001118@gmail.com>
2025-02-22 17:21:37 -0500

    relacion uno a muchos tablas categories y expenses

--- src/DAO/models/categories.js
+++ src/DAO/models/categories.js
@@ -10,7 +10,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Categories.hasMany(models.Expenses, {
+        foreignKey: 'category_id',
+        as: 'expenses'
+      });
     }
   }
   Categories.init({
