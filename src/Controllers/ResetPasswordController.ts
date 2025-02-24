ddfe489e90bc941cc2915941935e3a29d9947e00
Erling2 <20223058@aloe.ulima.edu.pe>
2025-02-23 19:00:35 -0500

    creando endpoint para cambio de contraseña

--- src/Controllers/ResetPasswordController.ts
+++ src/Controllers/ResetPasswordController.ts
@@ -1,11 +1,40 @@
 import express, {Request, Response} from "express"
 const db = require("../DAO/models")
-const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
 
 const ResetPasswordController = () => {
     const path: string = "/reset-password";
     
     const router = express.Router();
+
+    // Endpoint para cambiar la constraseña
+    router.post('/', async (req: Request, res: Response) => {
+
+        const { email, password, rw_password } = req.body;
+
+        const newPasswordCorrect = password === rw_password ? true : false
+        const userExists = await db.Usuario.findOne({ where: { email } });
+        
+
+        if ((newPasswordCorrect && userExists)) {
+
+            res.json({
+                msg: ""
+            })
+
+            const newPasswordHash = await bcrypt.hash(password, 10);
+            await db.Usuario.update(
+                { password_hash: newPasswordHash }, // Valores a actualizar
+                { where: { email } } // Condición para encontrar al usuario
+              );
+
+        } else {
+            res.json({
+                msg: "Contraseñas o usuario incorrectos"
+            })
+        }
+
+    });
 
     return [path, router]
 
