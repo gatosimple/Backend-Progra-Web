78f9fc2fbb66ca8a167ee265a829d075e8b27510
fabricio <20224387@aloe.ulima.edu.pe>
2025-02-25 20:23:11 -0500

    Fabricio_2023

--- src/DAO/models/access_logs.js
+++ src/DAO/models/access_logs.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       Access_logs.belongsTo(models.Usuario, {
         foreignKey: 'user_id',
-        as: 'usuario'
+        as: 'Usuario'
       });
     }
   }
