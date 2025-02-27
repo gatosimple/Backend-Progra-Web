9b6d6c7fd9387c7d4370a0160f55263f09d4d5b2
Erling2 <20223058@aloe.ulima.edu.pe>
2025-02-26 21:48:41 -0500

    Actualización de tabla password-reset

--- src/DAO/migrations/20250224214150-create-password-resets.js
+++ src/DAO/migrations/20250224214150-create-password-resets.js
@@ -23,7 +23,8 @@ module.exports = {
         type: Sequelize.STRING
       },
       created_at: {
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('NOW')
       }
     });
   },
