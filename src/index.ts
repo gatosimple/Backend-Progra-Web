9a2eb89b2c0bd81c749b04d67f6494663547119d
Enzo_Ordoñez <enzoordonezf@gmail.com>
2025-03-11 01:17:37 -0500

    solucion total de la nube

--- src/index.ts
+++ src/index.ts
@@ -22,12 +22,13 @@ import UserStatsController from "./Controllers/UserStatsController";
 dotenv.config()
 
 const app : Express = express()
+app.use(cors({ origin: "*" }));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
     extended : true
 }))
 app.use(express.static("assets")) // Carpeta archivos estaticos
-app.use(cors()) // TODO: Incrementar la seguridad
+
 
 const port = process.env.PORT || 5000;
 
